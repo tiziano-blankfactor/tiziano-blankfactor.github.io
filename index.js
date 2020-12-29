e1ecefd3ddfb91a7d1bfa531a93ff808c3d368b8
tiziano-blankfactor <76168939+tiziano-blankfactor@users.noreply.github.com>
2020-12-29 09:25:24 -0500

    fix

--- index.js
+++
@@ -1,8 +0,0 @@
-function Payment() {
-
-    _self.window.plugins.Toaster.show('Bienvenido a Prontomas', 'long', ()=> {
-        console.log('Native toast displayed')
-    }, ()=> {
-        console.log('Native toast failed')
-    })
-};
